434c01d0185ef540735c840f51effc5dd4c912cf
AI-NOMIS2 <paul.ai.nomis@gmail.com>
2022-02-08 14:12:24 +0900

    crud 까지 설정

---
+++ src/main/java/com/boottest/springboot/web/IndexController.java
@@ -0,0 +1,39 @@
+package com.boottest.springboot.web;
+
+
+import com.boottest.springboot.service.posts.PostsService;
+import com.boottest.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+
+        PostsResponseDto dto =postsService.findById(id);
+        model.addAttribute("post",dto);
+        System.out.println("index controller = "+dto.toString());
+        return "posts-update";
+    }
+}
---
+++ src/main/java/com/boottest/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,25 @@
+package com.boottest.springboot.web.dto;
+
+
+import com.boottest.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+
+
+}
